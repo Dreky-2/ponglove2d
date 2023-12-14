deffefa780a02f12a0353f1e4ccbf01d7621b289
intsutop <intsutop@gmail.com>
2023-12-14 08:38:25 +0200

    .

--- New Text Document.bat
+++
@@ -1,5 +0,0 @@
-cd "src"
-"C:\Program Files\7-Zip\7z.exe" a -tzip ..\src.zip *
-cd ..
-call build.bat src.zip
-pause
--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-copy /b love.exe+%1 "%~n1.exe"
+cd "src"
+"C:\Program Files\7-Zip\7z.exe" a -tzip ..\src.zip *
+cd ..
+call cmp.bat src.zip
+pause
